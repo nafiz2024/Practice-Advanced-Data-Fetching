7ce43812bc159e9be52cc110e3a7c75ba5c33a6e
Nafiz Alam <nafizalam.dev@gmail.com>
2026-04-23 05:03:49 +0600

    Add Force cache for cache data on website

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,6 @@
 const getProducts = async () => {
     try {
-        const res = await fetch('http://localhost:5000/products')
+        const res = await fetch('http://localhost:5000/products', { cache: 'force-cache' })
         return res.json();
     }
     catch (err) {
