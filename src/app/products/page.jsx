a58a80c0a7d3f4489bb61dc95b43207668020f58
Nafiz Alam <nafizalam.dev@gmail.com>
2026-04-23 05:15:56 +0600

    Dynamic Data Fetching cache no-store and revalidate

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,6 @@
 const getProducts = async () => {
     try {
-        const res = await fetch('http://localhost:5000/products', { cache: 'force-cache' })
+        const res = await fetch('http://localhost:5000/products', { cache: 'no-store' })
         return res.json();
     }
     catch (err) {
